eb2f9d1b0ad6ea91c2c600e8cfbce0900fb8833f
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-07 14:39:56 -0500

    feat(dashboard): add Dashboard and Header components

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+import { Header } from "@sections/dashboard/Header/Header";
+
+export const Dashboard = () => {
+  return <section>
+    <Header />
+  </section>;
+};
---
+++ src/sections/dashboard/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { LogOut } from "lucide-react";
+
+export const Header = () => {
+  return (
+    <header className="w-full border-b border-[#e4e4e7] bg-white shadow-sm">
+      <div className="mx-auto flex max-w-screen-xl items-center justify-between p-4">
+        <h1 className="inline-flex bg-gradient-to-r from-[#A3A3A3] via-[#D4AF37] to-[#A3A3A3] bg-clip-text text-2xl font-bold text-transparent">
+          Control 4x4
+        </h1>
+        <button className="flex h-10 cursor-pointer items-center gap-2 rounded-md px-4 py-2 transition-colors duration-300 hover:bg-[#f4f4f5]">
+          <LogOut size={16} />
+          <span className="text-sm font-medium text-[#18181b]">
+            Cerrar sesión
+          </span>
+        </button>
+      </div>
+    </header>
+  );
+};

7fd47d7b14bdaa8d1b6586907436d13f5f33c7f9
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-06 19:21:46 -0500

    feat(components): add SummaryCard component

---
+++ src/components/SummaryCard/SummaryCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface SummaryCardProps {
+  icon: React.ElementType;
+  title: "Total Importación" | "Saldo de Cupo" | "Cupo Anual";
+  amount: number;
+}
+
+export const SummaryCard = ({ icon, title, amount }: SummaryCardProps) => {
+  return (
+    <div className="w-full rounded-[8px] border border-[#E4E4E7] bg-white p-6 shadow-sm">
+      <div className="space-y-2">
+        <p className="flex items-center gap-2.5 font-medium text-[#52525B]">
+          {React.createElement(icon, {
+            className: `size-5 ${title == "Total Importación" && "text-[#18181B]"} ${title == "Saldo de Cupo" && "text-[#4ADE80]"} ${title == "Cupo Anual" && "text-[#D4AF37]"}`,
+          })}
+          {title}
+        </p>
+        <p
+          className={`text-xl font-bold ${title == "Total Importación" && "text-[#18181B]"} ${title == "Saldo de Cupo" && "text-[#4ADE80]"} ${title == "Cupo Anual" && "text-[#D4AF37]"}`}
+        >
+          ${amount}
+        </p>
+      </div>
+    </div>
+  );
+};

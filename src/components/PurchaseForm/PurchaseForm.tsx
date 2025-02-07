44b57db9f863398933e1682fd4772289408ab6af
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-07 17:53:44 -0500

    feat(PurchaseForm): add PurchaseForm component with input fields for date, tracking number, store, and total amount

---
+++ src/components/PurchaseForm/PurchaseForm.tsx
@@ -0,0 +1,57 @@
+import { FormButton } from "@components/FormButton/FormButton";
+import { FormInput } from "@components/FormInput/FormInput";
+import { FormLabel } from "@components/FormLabel/FormLabel";
+import { Barcode, Calendar, DollarSign, Store } from "lucide-react";
+
+export const PurchaseForm = () => {
+  return (
+    <div className="rounded-lg bg-white p-6 ring shadow-sm ring-[#E4E4E7]">
+      <h2 className="mb-4 text-lg font-semibold">Nueva Compra</h2>
+      <form action="" className="flex flex-col gap-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <div className="space-y-4">
+            <div className="flex flex-col gap-2.5">
+              <FormLabel htmlFor="date" text="Fecha" />
+              <FormInput
+                type="date"
+                id="date"
+                placeholder="Fecha"
+                icon={Calendar}
+              />
+            </div>
+            <div className="flex flex-col gap-2.5">
+              <FormLabel htmlFor="tracking-number" text="Tracking" />
+              <FormInput
+                type="text"
+                id="tracking-number"
+                placeholder="Tracking"
+                icon={Barcode}
+              />
+            </div>
+          </div>
+          <div className="space-y-4">
+            <div className="flex flex-col gap-2.5">
+              <FormLabel htmlFor="store" text="Tienda" />
+              <FormInput
+                type="text"
+                id="store"
+                placeholder="Tienda"
+                icon={Store}
+              />
+            </div>
+            <div className="flex flex-col gap-2.5">
+              <FormLabel htmlFor="amount" text="Valor total" />
+              <FormInput
+                id="amount"
+                type="number"
+                placeholder="Valor total"
+                icon={DollarSign}
+              />
+            </div>
+          </div>
+        </div>
+        <FormButton type="internal" text="Agregar compra" />
+      </form>
+    </div>
+  );
+};

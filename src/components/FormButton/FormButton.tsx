a27385b7c0d9098ad1d5e18014f198416abebeb6
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-07 14:18:33 -0500

    style(components): enhance FormButton styles with ring effect

--- src/components/FormButton/FormButton.tsx
+++ src/components/FormButton/FormButton.tsx
@@ -11,7 +11,7 @@ export const FormButton = ({ type, text }: FormButtonProps) => {
       {text}
     </button>
   ) : (
-    <button className="flex w-full cursor-pointer items-center justify-center gap-2.5 rounded-md border border-[#9394A5] bg-[#FFFFFF] px-4 py-2 text-base font-medium text-[#9394A5] transition-colors duration-300 hover:bg-[#FAFAFA]">
+    <button className="flex w-full cursor-pointer items-center justify-center gap-2.5 rounded-md bg-[#FFFFFF] px-4 py-2 text-base font-medium text-[#9394A5] ring ring-[#9394A5] transition-colors duration-300 hover:bg-[#FAFAFA]">
       <img src={Google} alt="Google" className="size-5" />
       {text}
     </button>

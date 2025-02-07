c8953dbba83a387251d9e909c76acf2cd08b0185
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-07 17:55:21 -0500

    feat(Routing): implement routing for Login, Register, and Dashboard pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import { App } from "./App";
+import { LoginPage } from "@pages/LoginPage/LoginPage";
+import { RegisterPage } from "@pages/RegisterPage/RegisterPage";
+import { Dashboard } from "@pages/Dashboard/Dashboard";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 );

65047142b2fae8165191d2996a64a5fdef5079c3
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-07 11:13:26 -0500

    feat(style): add height to maintain consistency
    Now the inputs keep the same height as the form buttons

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -18,7 +18,7 @@ export const FormInput = ({ id, type, placeholder, icon }: FormInputProps) => {
         id={id}
         type={type}
         placeholder={placeholder}
-        className="flex w-full rounded-md border border-[#9394A5]/50 bg-[#FAFAFA] py-2 pr-3 pl-10 text-sm"
+        className="flex h-10 w-full rounded-md border border-[#9394A5]/50 bg-[#FAFAFA] py-2 pr-3 pl-10 text-sm"
       />
     </div>
   );

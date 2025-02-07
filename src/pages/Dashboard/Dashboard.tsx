7846fea284a93d9ea7ca9109988b246c1752a28c
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-07 17:54:19 -0500

    feat(Dashboard): integrate DashboardLayout with Summary, PurchaseForm, and PurchaseTable components

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,18 @@
 import { Header } from "@sections/dashboard/Header/Header";
+import { DashboardLayout } from "@layouts/DashboardLayout/DashboardLayout";
+import { Summary } from "@components/Summary/Summary";
+import { PurchaseForm } from "@components/PurchaseForm/PurchaseForm";
+import { PurchaseTable } from "@components/PurchaseTable/PurchaseTable";
 
 export const Dashboard = () => {
-  return <section>
-    <Header />
-  </section>;
+  return (
+    <>
+      <Header />
+      <DashboardLayout>
+        <Summary />
+        <PurchaseForm />
+        <PurchaseTable />
+      </DashboardLayout>
+    </>
+  );
 };

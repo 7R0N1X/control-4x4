3e030db89c71dadc5110690060c67b6ed319a4d2
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-07 14:49:40 -0500

    style(SummaryCard): adjust font size for title and amount in SummaryCard component

--- src/components/SummaryCard/SummaryCard.tsx
+++ src/components/SummaryCard/SummaryCard.tsx
@@ -10,14 +10,14 @@ export const SummaryCard = ({ icon, title, amount }: SummaryCardProps) => {
   return (
     <div className="w-full rounded-[8px] border border-[#E4E4E7] bg-white p-6 shadow-sm">
       <div className="space-y-2">
-        <p className="flex items-center gap-2.5 font-medium text-[#52525B]">
+        <p className="flex items-center gap-2.5 text-sm font-medium text-[#52525B]">
           {React.createElement(icon, {
             className: `size-5 ${title == "Total Importación" && "text-[#18181B]"} ${title == "Saldo de Cupo" && "text-[#4ADE80]"} ${title == "Cupo Anual" && "text-[#D4AF37]"}`,
           })}
           {title}
         </p>
         <p
-          className={`text-xl font-bold ${title == "Total Importación" && "text-[#18181B]"} ${title == "Saldo de Cupo" && "text-[#4ADE80]"} ${title == "Cupo Anual" && "text-[#D4AF37]"}`}
+          className={`text-2xl font-bold ${title == "Total Importación" && "text-[#18181B]"} ${title == "Saldo de Cupo" && "text-[#4ADE80]"} ${title == "Cupo Anual" && "text-[#D4AF37]"}`}
         >
           ${amount}
         </p>

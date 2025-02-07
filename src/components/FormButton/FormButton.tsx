cae8ca8ea54d1158c998b6bce5b1e4d97eac0484
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-07 10:53:16 -0500

    feat(components): add FormButton component

---
+++ src/components/FormButton/FormButton.tsx
@@ -0,0 +1,19 @@
+import Google from "@assets/icons/Google.svg";
+
+interface FormButtonProps {
+  type: "internal" | "external";
+  text: string;
+}
+
+export const FormButton = ({ type, text }: FormButtonProps) => {
+  return type === "internal" ? (
+    <button className="flex w-full cursor-pointer items-center justify-center gap-2.5 rounded-md bg-[#3C3C3B] px-4 py-2 text-base font-medium text-[#D4AF37] transition-colors duration-300 hover:bg-[#242424]">
+      {text}
+    </button>
+  ) : (
+    <button className="flex w-full cursor-pointer items-center justify-center gap-2.5 rounded-md border border-[#9394A5] bg-[#FFFFFF] px-4 py-2 text-base font-medium text-[#9394A5] transition-colors duration-300 hover:bg-[#FAFAFA]">
+      <img src={Google} alt="Google" className="size-5" />
+      {text}
+    </button>
+  );
+};

ecde9dac3201dfcecc1d6ac813b6ff1304b53dbb
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-07 14:11:15 -0500

    feat(config): add alias for pages

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       "@components": `${path.resolve(__dirname, "src/components")}`,
       "@assets": `${path.resolve(__dirname, "src/assets")}`,
       "@layouts": `${path.resolve(__dirname, "src/layouts")}`,
+      "@pages": `${path.resolve(__dirname, "src/pages")}`,
     },
   },
 });

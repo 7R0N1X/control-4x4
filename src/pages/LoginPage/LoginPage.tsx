680f62c3b4eed252e0970b20acbbc9228af9519c
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-11 14:41:46 -0500

    feat(LoginPage): add Google sign-in button and handler

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -7,7 +7,10 @@ import { LockIcon, Mail } from "lucide-react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@store/store";
-import { signInWithEmailAndPasswordThunk } from "@store/auth/authThunk";
+import {
+  signInWithEmailAndPasswordThunk,
+  signInWithGoogleThunk,
+} from "@store/auth/authThunk";
 
 type LoginForm = {
   email: string;
@@ -23,55 +26,59 @@ export const LoginPage = () => {
     dispatch(signInWithEmailAndPasswordThunk(email, password));
     reset();
   };
+
+  const onClick = () => {
+    dispatch(signInWithGoogleThunk());
+  };
+
   return (
     <AuthLayout
       title="Ingreso a plataforma"
       description="Ingresa tus credenciales para continuar"
     >
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="mt-8 w-full max-w-[450px] space-y-4 rounded-lg p-6 ring shadow-sm ring-[#9394A5]/20"
-      >
-        <div className="flex flex-col gap-2">
-          <FormLabel htmlFor="email" text="Correo Electrónico" />
-          <FormInput
-            id="email"
-            type="email"
-            placeholder="Correo Electrónico"
-            icon={Mail}
-            {...register("email")}
-          />
-        </div>
+      <div className="mt-8 w-full max-w-[450px] space-y-4 rounded-lg p-6 ring shadow-sm ring-[#9394A5]/20">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div className="flex flex-col gap-2">
+            <FormLabel htmlFor="email" text="Correo Electrónico" />
+            <FormInput
+              id="email"
+              type="email"
+              placeholder="Correo Electrónico"
+              icon={Mail}
+              {...register("email")}
+            />
+          </div>
 
-        <div className="flex flex-col gap-2">
-          <FormLabel htmlFor="password" text="Contraseña" />
-          <FormInput
-            id="password"
-            type="password"
-            placeholder="Contraseña"
-            icon={LockIcon}
-            {...register("password")}
-          />
-        </div>
+          <div className="flex flex-col gap-2">
+            <FormLabel htmlFor="password" text="Contraseña" />
+            <FormInput
+              id="password"
+              type="password"
+              placeholder="Contraseña"
+              icon={LockIcon}
+              {...register("password")}
+            />
+          </div>
 
-        <FormLink variant="right" href="" text="¿Olvidaste tu contraseña?" />
+          <FormLink variant="right" href="" text="¿Olvidaste tu contraseña?" />
 
-        <FormButton type="internal" text="Iniciar Sesión" />
+          <FormButton type="internal" text="Iniciar Sesión" />
 
-        <div className="flex items-center gap-2.5">
-          <div className="h-[1px] w-full bg-[#D9D9D9]"></div>
-          <span className="font-medium text-[#9394A5]">o</span>
-          <div className="h-[1px] w-full bg-[#D9D9D9]"></div>
-        </div>
+          <div className="flex items-center gap-2.5">
+            <div className="h-[1px] w-full bg-[#D9D9D9]"></div>
+            <span className="font-medium text-[#9394A5]">o</span>
+            <div className="h-[1px] w-full bg-[#D9D9D9]"></div>
+          </div>
+        </form>
 
-        <FormButton type="external" text="Continuar con Google" />
+        <FormButton onClick={onClick} type="external" text="Continuar con Google" />
 
         <FormLink
           variant="center"
           href="/register"
           text="¿No tienes una cuenta? Regístrate"
         />
-      </form>
+      </div>
     </AuthLayout>
   );
 };

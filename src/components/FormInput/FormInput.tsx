101abd231bafddd3a35ab4eacce9004008c221e7
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-11 10:00:09 -0500

    feat(FormInput): make id prop optional and spread additional props

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import "./FormInput.css";
 
 interface FormInputProps {
-  id: string;
+  id?: string;
   type: string;
   placeholder: string;
   icon: React.ElementType;
 }
 
-export const FormInput = ({ id, type, placeholder, icon }: FormInputProps) => {
+export const FormInput = ({
+  id,
+  type,
+  placeholder,
+  icon,
+  ...props
+}: FormInputProps) => {
   return (
     <div className="relative inline-block">
       {React.createElement(icon, {
@@ -20,6 +26,7 @@ export const FormInput = ({ id, type, placeholder, icon }: FormInputProps) => {
         type={type}
         placeholder={placeholder}
         className="flex h-10 w-full rounded-md border border-[#9394A5]/50 bg-white py-2 pr-3 pl-10 text-sm"
+        {...props}
       />
     </div>
   );

b3fb3e3baebfb34dd3f68729202c668703d70e61
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-11 10:00:36 -0500

    feat(main): wrap application in Redux Provider for state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import { LoginPage } from "@pages/LoginPage/LoginPage";
-import { RegisterPage } from "@pages/RegisterPage/RegisterPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import { Dashboard } from "@pages/Dashboard/Dashboard";
+import { LoginPage } from "@pages/LoginPage/LoginPage";
+import { Provider } from "react-redux";
+import { RegisterPage } from "@pages/RegisterPage/RegisterPage";
+import { store } from "@store/store";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );

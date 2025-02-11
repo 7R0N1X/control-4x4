89659d7b6539cf443a87344cfe23b2469c99dd86
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-11 15:01:28 -0500

    refactor(main): replace Routes with AppRouter for improved routing structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppRouter } from "./router/AppRouter";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import { Dashboard } from "@pages/Dashboard/Dashboard";
-import { LoginPage } from "@pages/LoginPage/LoginPage";
 import { Provider } from "react-redux";
-import { RegisterPage } from "@pages/RegisterPage/RegisterPage";
 import { store } from "@store/store";
 import { StrictMode } from "react";
 
@@ -12,11 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
+        <AppRouter />
       </BrowserRouter>
     </Provider>
   </StrictMode>,

ad7db6a765d24155f99b717843e43d1fb1347167
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-11 17:30:23 -0500

    feat(logo): add optional className prop for customizable styling

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,6 +1,12 @@
-export const Logo = () => {
+interface LogoProps {
+  className?: string;
+}
+
+export const Logo = ({ className }: LogoProps) => {
   return (
-    <h1 className="mb-6 inline-flex bg-gradient-to-r from-zinc-600 via-[#D4AF37] to-zinc-600 bg-clip-text text-2xl font-bold text-transparent">
+    <h1
+      className={`${className} inline-flex bg-gradient-to-r from-zinc-600 via-[#D4AF37] to-zinc-600 bg-clip-text text-2xl font-bold text-transparent`}
+    >
       Control 4x4
     </h1>
   );

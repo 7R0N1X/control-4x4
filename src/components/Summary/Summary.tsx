912c3d7604d6097bd7d658c2bd2fb67623049275
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-13 09:47:53 -0500

    feat(summary): update Summary component to include new ShoppingCart icon and adjust titles

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,11 +1,12 @@
 import { SummaryCard } from "@components/SummaryCard/SummaryCard";
-import { CoinsIcon, Package, TrendingUp } from "lucide-react";
+import { CoinsIcon, Package, ShoppingCart, TrendingUp } from "lucide-react";
 
 export const Summary = () => {
   return (
-    <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-3">
-      <SummaryCard icon={Package} title="Total Importación" amount={1000} />
-      <SummaryCard icon={CoinsIcon} title="Saldo de Cupo" amount={1000} />
+    <div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-3 xl:grid-cols-4">
+      <SummaryCard icon={Package} title="Saldo Ocupado" amount={1000} />
+      <SummaryCard icon={CoinsIcon} title="Saldo Disponible" amount={1000} />
+      <SummaryCard icon={ShoppingCart} title="Compras Realizadas" amount={0} />
       <SummaryCard icon={TrendingUp} title="Cupo Anual" amount={1000} />
     </div>
   );

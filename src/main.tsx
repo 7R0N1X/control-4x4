d3924c068b3680db30d2c9beb8881c82b6d695a0
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-13 12:19:39 -0500

    feat(auth): initialize authListener in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import "./index.css";
 import { AppRouter } from "./router/AppRouter";
+import { authListener } from "@utils/authListener";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "@store/store";
 import { StrictMode } from "react";
+
+authListener(store.dispatch);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

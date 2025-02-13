11ad10269ccc0b57464fb00fee5bf72d6707c59d
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-13 18:41:25 -0500

    feat(configuration): add Configuration component with DashboardLayout

---
+++ src/pages/Configuration/Configuration.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from "@layouts/DashboardLayout/DashboardLayout";
+
+export const Configuration = () => {
+  return (
+    <DashboardLayout>
+      <h2>Configuración</h2>
+    </DashboardLayout>
+  );
+};

7c66be111a59699065933dbba35c810a645134ef
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-13 12:19:28 -0500

    feat(router): add loading spinner during authentication state changes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,11 +3,19 @@ import { DashboardRouter } from "./DashboardRouter";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { RootState } from "@store/store";
 import { useSelector } from "react-redux";
+import { BeatLoader } from "react-spinners";
 
 export const AppRouter = () => {
   const isAuthenticated = useSelector(
     (state: RootState) => state.auth.isAuthenticated,
   );
+
+  const isLoading = useSelector((state: RootState) => state.auth.isLoading);
+
+  if (isLoading) {
+    return <BeatLoader color="#d4af37" />;
+  }
+
   return (
     <Routes>
       {isAuthenticated ? (

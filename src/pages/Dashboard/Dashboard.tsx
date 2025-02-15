2a41968d3703aa8d778cf7860e3cdc564fd4aca5
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-15 17:05:21 -0500

    feat(Dashboard): add Navbar component to Dashboard layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,10 +2,12 @@ import { DashboardLayout } from "@layouts/DashboardLayout/DashboardLayout";
 import { Summary } from "@components/Summary/Summary";
 import { PurchaseForm } from "@components/PurchaseForm/PurchaseForm";
 import { PurchaseTable } from "@components/PurchaseTable/PurchaseTable";
+import { Navbar } from "@components/Navbar/Navbar";
 
 export const Dashboard = () => {
   return (
     <DashboardLayout>
+      <Navbar />
       <Summary />
       <PurchaseForm />
       <PurchaseTable />

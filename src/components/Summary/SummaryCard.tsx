7148e82ad4964a8acc607b2e3c7744dd3db38ec2
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-15 17:41:53 -0500

    feat(SummaryCard): enhance component with subtitle and dynamic styling

--- src/components/Summary/SummaryCard.tsx
+++ src/components/Summary/SummaryCard.tsx
@@ -1,61 +1,34 @@
 import React from "react";
-import classNames from "classnames";
+import { TrendingUp } from "lucide-react";
 
 interface SummaryCardProps {
   icon: React.ElementType;
-  title:
-    | "Saldo Ocupado"
-    | "Saldo Disponible"
-    | "Cupo Anual"
-    | "Compras Realizadas";
+  title: "Cupo Anual" | "Saldo Disponible" | "Total Importado";
+  subtitle:
+    | "Límite establecido"
+    | "Disponible para compras"
+    | "Valor acumulado";
   amount: number;
 }
 
-const styles = {
-  "Saldo Ocupado": {
-    text: "text-[#18181B]",
-    bg: "bg-[#18181B]",
-    iconBg: "bg-zinc-100",
-    iconText: "text-[#18181B]",
-  },
-  "Saldo Disponible": {
-    text: "text-[#4ade80]",
-    bg: "bg-[#4ade80]",
-    iconBg: "bg-[#4ade801a]",
-    iconText: "text-[#4ade80]",
-  },
-  "Cupo Anual": {
-    text: "text-[#d4af37]",
-    bg: "bg-[#d4af37]",
-    iconBg: "bg-[#d4af371a]",
-    iconText: "text-[#d4af37]",
-  },
-  "Compras Realizadas": {
-    text: "text-blue-500",
-    bg: "bg-blue-500",
-    iconBg: "bg-[#3B82F61a]",
-    iconText: "text-blue-500",
-  },
-};
-
-export const SummaryCard = ({ icon, title, amount }: SummaryCardProps) => {
-  const { text, bg, iconBg, iconText } = styles[title];
-
+export const SummaryCard = ({
+  icon,
+  title,
+  subtitle,
+  amount,
+}: SummaryCardProps) => {
   return (
-    <div className="w-full rounded-lg bg-white p-6 ring shadow-sm ring-[#E4E4E7]">
-      <div className="flex h-full w-full items-center gap-3">
-        <div className={classNames("min-h-full w-1", bg)} />
-        <div className={classNames("rounded-md p-2", iconBg)}>
-          {React.createElement(icon, {
-            className: classNames("size-5", iconText),
-          })}
-        </div>
-        <div>
-          <p className="flex items-center gap-2.5 text-sm font-medium text-[#52525B]">
-            {title}
-          </p>
-          <p className={classNames("text-xl font-bold", text)}>${amount}</p>
-        </div>
+    <div
+      className={`text w-full rounded-lg p-6 text-white ring shadow-sm ring-[#E4E4E7] ${title === "Cupo Anual" ? "bg-gradient-to-br from-[#D4AF37] to-[#b99930] text-white" : ""} ${title === "Saldo Disponible" ? "bg-gradient-to-br from-emerald-600 to-emerald-700" : ""} ${title === "Total Importado" ? "bg-gradient-to-br from-zinc-600 to-zinc-800" : ""}`}
+    >
+      <div className="mb-4 flex items-center justify-between">
+        <h2 className="text-lg font-semibold">{title}</h2>
+        {React.createElement(icon, { size: 32 })}
+      </div>
+      <p className="mb-2 text-3xl font-bold">${amount}</p>
+      <div className="flex items-center text-gray-100">
+        <TrendingUp size={16} className="mr-1" />
+        <span className="text-sm">{subtitle}</span>
       </div>
     </div>
   );

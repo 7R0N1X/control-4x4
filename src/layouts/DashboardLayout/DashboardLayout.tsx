999891d84144a8d5f1e60ad97ecbe3b8f166a424
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-15 18:03:14 -0500

    fix(DashboardLayout): add padding for improved layout on larger screens

--- src/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -6,7 +6,9 @@ export const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <section className="flex min-h-screen w-full bg-[#FAFAFA]">
       <div className="mx-auto w-full max-w-[1400px] pt-[68px]">
-        <div className="mx-auto flex w-full flex-col gap-6 p-4">{children}</div>
+        <div className="mx-auto flex w-full flex-col gap-6 p-4 lg:pt-6">
+          {children}
+        </div>
       </div>
     </section>
   );

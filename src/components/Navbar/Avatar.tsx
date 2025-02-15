8979f59b32f0e712a731d4f8fd65fd0cb9917a9c
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-15 17:05:05 -0500

    feat(Navbar): add Avatar component for user profile and logout functionality

---
+++ src/components/Navbar/Avatar.tsx
@@ -0,0 +1,54 @@
+import { logOut } from "@store/auth/authThunk";
+import { AppDispatch, RootState } from "@store/store";
+import { ChevronDown, LogOut } from "lucide-react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export const Avatar = () => {
+  const { displayName, photoURL } = useSelector(
+    (state: RootState) => state.auth.auth,
+  );
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <button
+      className="flex cursor-pointer items-center gap-2.5"
+      onClick={handleOpen}
+    >
+      <picture className="size-9 overflow-hidden rounded-full bg-white">
+        <img
+          src={photoURL ? photoURL : ""}
+          alt={`Foto de ${displayName}`}
+          className="size-9 object-cover"
+        />
+      </picture>
+
+      <div className="relative flex flex-1 gap-2.5">
+        <span className="font-medium">{displayName}</span>
+        <ChevronDown />
+
+        {isOpen && (
+          <div className="absolute top-10 right-0 min-w-[max-content] rounded-md bg-white p-1 ring shadow-md ring-[#E4E4E7]">
+            <button
+              onClick={handleLogout}
+              className="flex cursor-pointer items-center gap-2 px-2 py-1.5 text-sm font-medium text-red-600 transition-colors duration-300 hover:text-[#18181b]"
+            >
+              <LogOut size={14} /> Cerrar Sesión
+            </button>
+          </div>
+        )}
+      </div>
+    </button>
+  );
+};
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Logo } from "@components/Logo/Logo";
+import { Link } from "react-router-dom";
+import { Avatar } from "./Avatar";
+
+export const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full bg-white shadow-sm">
+      <div className="mx-auto flex w-full max-w-[1400px] items-center justify-between p-4">
+        <Link to={"/"}>
+          <Logo />
+        </Link>
+        <Avatar />
+      </div>
+    </header>
+  );
+};

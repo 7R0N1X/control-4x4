b4fcdd750d28318b035029e64769a1fccd07dfbb
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-15 16:14:34 -0500

    fix(router): remove configuration route from DashboardRouter

--- src/router/DashboardRouter.tsx
+++ src/router/DashboardRouter.tsx
@@ -1,12 +1,10 @@
 import { Dashboard } from "@pages/Dashboard/Dashboard";
-import { Configuration } from "@pages/Configuration/Configuration";
 import { Navigate, Routes, Route } from "react-router-dom";
 
 export const DashboardRouter = () => {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/dashboard/configuration" element={<Configuration />} />
       <Route path="/*" element={<Navigate to={"/dashboard"} />} />
     </Routes>
   );

cb1fa87c5af470d492bdb8f2e7f2eeec8051f59e
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-15 16:14:57 -0500

    refactor(sidebar): remove Avatar, NavItem, Sidebar, SidebarFooter, and SidebarNavigation components

--- src/pages/Configuration/Configuration.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardLayout } from "@layouts/DashboardLayout/DashboardLayout";
-
-export const Configuration = () => {
-  return (
-    <DashboardLayout>
-      <h2>Configuración</h2>
-    </DashboardLayout>
-  );
-};

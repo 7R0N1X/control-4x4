b585fc6e0de28472c4dbf983ea4b07a34f137d8c
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-16 12:21:08 -0500

    feat(user): add getPurchases thunk to retrieve user purchase history

--- src/store/user/userThunk.ts
+++ src/store/user/userThunk.ts
@@ -1,20 +1,39 @@
-import { db } from "@/firebase/config";
-import { collection, addDoc } from "firebase/firestore/lite";
 import { AppDispatch, RootState } from "@store/store";
-import { setPurchase } from "./userSlice";
+import { collection, addDoc, getDocs } from "firebase/firestore/lite";
+import { db } from "@/firebase/config";
+import { loadPurchases, setPurchase } from "./userSlice";
 
 export const addNewPurchase = (data: any) => {
   return async (dispatch: AppDispatch, getState: () => RootState) => {
-    const {
-      auth: { uid },
-    } = getState().auth;
-
     try {
-      const docRef = await addDoc(collection(db, `users/${uid}/purchases`), {
+      const { uid } = getState().auth.auth;
+      const docRef = await addDoc(
+        collection(db, `users/${uid}/purchases`),
         data,
-      });
+      );
 
       dispatch(setPurchase({ ...data, id: docRef.id }));
+    } catch (error) {
+      throw error;
+    }
+  };
+};
+
+export const getPurchases = () => {
+  return async (dispatch: AppDispatch, getState: () => RootState) => {
+    try {
+      const { uid } = getState().auth.auth;
+      const purchasesRef = collection(db, `users/${uid}/purchases`);
+      const querySnapshot = await getDocs(purchasesRef);
+
+      const purchases: any[] = [];
+      querySnapshot.forEach((docSnap: any) => {
+        const _data = docSnap.data();
+        const { data } = _data;
+        purchases.push({ ...data, id: docSnap.id });
+      });
+
+      dispatch(loadPurchases(purchases));
     } catch (error) {
       throw error;
     }

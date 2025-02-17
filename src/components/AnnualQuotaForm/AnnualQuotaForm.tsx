1223ae483f1357f10b21ee5bc5c09a45bbf2bbcc
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-17 12:08:26 -0500

    feat(annual-quota-form): make AnnualQuotaForm responsive with max width adjustment

--- src/components/AnnualQuotaForm/AnnualQuotaForm.tsx
+++ src/components/AnnualQuotaForm/AnnualQuotaForm.tsx
@@ -9,7 +9,7 @@ interface AnnualQuotaFormProps {
 
 export const AnnualQuotaForm = ({ onClose }: AnnualQuotaFormProps) => {
   return (
-    <div className="fixed top-[50%] left-[50%] z-50 w-[512px] translate-x-[-50%] translate-y-[-50%] rounded-lg border border-zinc-200 bg-white p-6 shadow-sm">
+    <div className="fixed top-[50%] left-[50%] z-50 w-[512px] translate-x-[-50%] translate-y-[-50%] rounded-lg border border-zinc-200 bg-white p-6 shadow-sm max-sm:w-full">
       <div className="mb-4 flex w-full items-center justify-between">
         <h2 className="text-lg font-semibold">Editar Cupo Anual</h2>
         <button className="cursor-pointer" onClick={onClose} title="Cerrar">

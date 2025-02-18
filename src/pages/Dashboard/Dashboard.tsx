012af6e65ada5a0fc1c07e2cee6a79876cca9bf7
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-18 11:08:56 -0500

    feat(Dashboard): refactor purchase loading logic in useEffect and cleanup

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
 import { AppDispatch } from "@store/store";
 import { DashboardLayout } from "@layouts/DashboardLayout/DashboardLayout";
-import { getPurchases } from "@store/user/userThunk";
+import { loadPurchases } from "@store/user/userSlice";
 import { Navbar } from "@components/Navbar/Navbar";
 import { PurchaseForm } from "@components/PurchaseForm/PurchaseForm";
 import { PurchaseTable } from "@components/PurchaseTable/PurchaseTable";
+import { readPurchasesThunk } from "@store/user/userThunk";
 import { Summary } from "@components/Summary/Summary";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
@@ -12,7 +13,11 @@ export const Dashboard = () => {
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    dispatch(getPurchases());
+    dispatch(readPurchasesThunk());
+
+    return () => {
+      dispatch(loadPurchases([]));
+    };
   }, []);
 
   return (

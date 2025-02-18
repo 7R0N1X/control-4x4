5a61d056aaa2db3f31f6606d9cea2c968178d1bf
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-18 11:43:13 -0500

    feat(Summary): update availableBalance and totalImported in Summary component

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,17 +1,24 @@
-import { SummaryCard } from "@components/Summary/SummaryCard";
 import { AppDispatch, RootState } from "@store/store";
 import { getAnnualQuotaThunk } from "@store/user/userThunk";
-import { Wallet, PiggyBank, CreditCard } from "lucide-react";
-import { useEffect } from "react";
+import { setAvailableBalance, setTotalImported } from "@store/user/userSlice";
+import { SummaryCard } from "@components/Summary/SummaryCard";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Wallet, PiggyBank, CreditCard } from "lucide-react";
 
 export const Summary = () => {
   const dispatch = useDispatch<AppDispatch>();
   const annualQuota = useSelector((state: RootState) => state.user.annualQuota);
-  
+  const { purchases, availableBalance, totalImported } = useSelector((state: RootState) => state.user);
+
   useEffect(() => {
     dispatch(getAnnualQuotaThunk());
   }, []);
+
+  useEffect(() => {
+    dispatch(setTotalImported(purchases));
+    dispatch(setAvailableBalance())
+  }, [purchases])
 
   return (
     <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 lg:gap-6">
@@ -25,13 +32,13 @@ export const Summary = () => {
         icon={PiggyBank}
         title="Saldo Disponible"
         subtitle="Disponible para compras"
-        amount={"0"}
+        amount={availableBalance.toFixed(2)}
       />
       <SummaryCard
         icon={CreditCard}
         title="Total Importado"
         subtitle="Valor acumulado"
-        amount={"0"}
+        amount={totalImported.toFixed(2)}
       />
     </div>
   );

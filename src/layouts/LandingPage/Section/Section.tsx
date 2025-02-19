148afe769cf07ea02d4ccc9218a3ca46c16ff4e7
7R0N1X <techsoftec.tronix@gmail.com>
2025-02-19 14:53:45 -0500

    feat(Section): create Section component with title, description, and children props

---
+++ src/layouts/LandingPage/Section/Section.tsx
@@ -0,0 +1,24 @@
+interface SectionProps {
+  children: React.ReactNode;
+  title?: string;
+  description?: string;
+  className?: string;
+}
+
+export const Section = ({ title, description, children, className }: SectionProps) => {
+  return (
+    <section className={`h-full w-full p-4 lg:p-0 ${className ? className : ""}`}>
+      <div className="mx-auto max-w-screen-xl">
+        {title && (
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h2 className="text-center text-3xl font-bold text-neutral-800">
+              {title}
+            </h2>
+            <p className="text-center text-lg text-[#606271]">{description}</p>
+          </div>
+        )}
+        {children}
+      </div>
+    </section>
+  );
+};

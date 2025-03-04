a02d7c4ace7bdedf76580bdd751915e2ff26282a
7R0N1X <techsoftec.tronix@gmail.com>
2025-03-04 09:30:23 -0500

    feat(PurchaseForm): add functionality to cancel purchase edit

--- src/components/PurchaseForm/PurchaseForm.tsx
+++ src/components/PurchaseForm/PurchaseForm.tsx
@@ -1,6 +1,6 @@
 import { createNewPurchase, updatePurchaseThunk } from "@store/user/userThunk";
 import { AppDispatch, RootState } from "@store/store";
-import { Barcode, Calendar, DollarSign, Store } from "lucide-react";
+import { Barcode, Calendar, DollarSign, Store, X } from "lucide-react";
 import { FormButton } from "@components/Form/FormButton";
 import { FormInput } from "@components/Form/FormInput/FormInput";
 import { FormLabel } from "@components/Form/FormLabel";
@@ -8,6 +8,7 @@ import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { useEffect } from "react";
+import { setIsEditing, setPurchaseEdit } from "@store/user/userSlice";
 
 type PurchaseFormData = {
   id: string;
@@ -38,7 +39,7 @@ export const PurchaseForm = () => {
   const onSubmit = async (data: PurchaseFormData) => {
     try {
       if (isEditing) {
-        const {store, trackingNumber, amount} = purchaseToEdit[0];
+        const { store, trackingNumber, amount } = purchaseToEdit[0];
         if (data.store !== store || data.trackingNumber !== trackingNumber || data.amount !== amount) {
           await dispatch(updatePurchaseThunk(purchaseToEdit[0].id, data));
           toast.success("Compra editada exitosamente");
@@ -55,9 +56,26 @@ export const PurchaseForm = () => {
     }
   };
 
+  const onCancelEdit = () => {
+    dispatch(setIsEditing(false));
+    dispatch(setPurchaseEdit(""));
+    reset();
+  };
+
   return (
     <div className="rounded-lg bg-white p-6 ring shadow-sm ring-[#E4E4E7]">
-      <h2 className="mb-4 text-lg font-semibold">Nueva Compra</h2>
+      <div className="flex w-full items-center justify-between">
+        <h2 className="mb-4 text-lg font-semibold">Nueva Compra</h2>
+        {isEditing && (
+          <button
+            onClick={onCancelEdit}
+            title="Cancelar edición"
+            className="flex cursor-pointer items-center gap-1 rounded-md px-4 py-2 text-sm ring ring-[#D4AF37] transition-colors duration-300 hover:bg-[#D4AF37] max-sm:hidden"
+          >
+            <X size={16} /> Cancelar edición
+          </button>
+        )}
+      </div>
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
         <div className="flex items-end justify-between gap-4 max-lg:flex-col">
           <div className="flex w-full flex-col gap-2.5">
@@ -104,7 +122,16 @@ export const PurchaseForm = () => {
               icon={DollarSign}
             />
           </div>
-          <FormButton type="internal" text={`${isEditing ? 'Guardar cambios' : 'Agregar compra'}`} />
+          <FormButton type="internal" text={`${isEditing ? "Guardar cambios" : "Agregar compra"}`} />
+          {isEditing && (
+            <button
+              onClick={onCancelEdit}
+              title="Cancelar edición"
+              className="flex w-full cursor-pointer items-center justify-center gap-1 rounded-md px-4 py-2 text-sm ring ring-[#D4AF37] transition-colors duration-300 hover:bg-[#D4AF37] sm:hidden"
+            >
+              <X size={16} /> Cancelar edición
+            </button>
+          )}
         </div>
       </form>
     </div>

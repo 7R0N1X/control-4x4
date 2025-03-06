a1e2602a55b37f27405034e52321d9ec9da9ea0f
7R0N1X <techsoftec.tronix@gmail.com>
2025-03-06 11:39:08 -0500

    feat(PurchaseForm): refactor form handling to use custom hook and streamline submission logic

--- src/components/PurchaseForm/PurchaseForm.tsx
+++ src/components/PurchaseForm/PurchaseForm.tsx
@@ -1,17 +1,14 @@
-import { createNewPurchase, updatePurchaseThunk } from "@store/user/userThunk";
+import { AmountField } from "@components/Form/FormInput/AmountField/AmountField";
 import { AppDispatch, RootState } from "@store/store";
-import { X } from "lucide-react";
-import { FormButton } from "@components/Form/FormButton";
-import { toast } from "react-toastify";
-import { useDispatch, useSelector } from "react-redux";
-import { useForm } from "react-hook-form";
-import { useEffect } from "react";
-import { setIsEditing, setPurchaseEdit } from "@store/user/userSlice";
 import { DateField } from "@components/Form/FormInput/DateField/DateField";
+import { formatDate } from "@utils/formatDate";
+import { FormButton } from "@components/Form/FormButton";
 import { StoreField } from "@components/Form/FormInput/StoreField/StoreField";
 import { TrackingField } from "@components/Form/FormInput/TrackingField/TrackingField";
-import { AmountField } from "@components/Form/FormInput/AmountField/AmountField";
-import { formatDate } from "@utils/formatDate";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { usePurchaseForm } from "./Hooks/usePurchaseForm";
+import { X } from "lucide-react";
 
 export type PurchaseFormData = {
   id: string;
@@ -23,16 +20,8 @@ export type PurchaseFormData = {
 
 export const PurchaseForm = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { purchaseToEdit, isEditing, availableBalance } = useSelector(
-    (state: RootState) => state.user,
-  );
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-    setValue,
-  } = useForm<PurchaseFormData>();
+  const { purchaseToEdit, isEditing, availableBalance } = useSelector((state: RootState) => state.user);
+  const { register, handleSubmit, errors, setValue, onSubmit, onCancelEdit } = usePurchaseForm({ dispatch, purchaseToEdit, isEditing, availableBalance });
 
   useEffect(() => {
     if (purchaseToEdit.length > 0) {
@@ -44,50 +33,12 @@ export const PurchaseForm = () => {
     }
   }, [purchaseToEdit]);
 
-  const onSubmit = async (data: PurchaseFormData) => {
-    try {
-      if (isEditing) {
-        const { store, trackingNumber, amount } = purchaseToEdit[0];
-        if (
-          data.store !== store ||
-          data.trackingNumber !== trackingNumber ||
-          data.amount !== amount
-        ) {
-          await dispatch(updatePurchaseThunk(purchaseToEdit[0].id, data));
-          toast.success("Compra editada exitosamente");
-        }
-        return;
-      } else {
-        if (availableBalance >= data.amount && data.amount != 0) {
-          await dispatch(createNewPurchase({...data, date: formatDate(data.date)}));
-          toast.success("Compra agregada exitosamente");
-          return;
-        }
-        toast.error("No cuenta con saldo disponible para esta transacción.");
-      }
-    } catch (error) {
-      toast.error("Error al agregar la compra");
-    } finally {
-      reset();
-    }
-  };
-
-  const onCancelEdit = () => {
-    dispatch(setIsEditing(false));
-    dispatch(setPurchaseEdit(""));
-    reset();
-  };
-
   return (
     <div className="rounded-lg bg-white p-6 ring shadow-sm ring-[#E4E4E7]">
       <div className="flex w-full items-center justify-between">
         <h2 className="mb-4 text-lg font-semibold">Nueva Compra</h2>
         {isEditing && (
-          <button
-            onClick={onCancelEdit}
-            title="Cancelar edición"
-            className="flex cursor-pointer items-center gap-1 rounded-md px-4 py-2 text-sm ring ring-[#D4AF37] transition-colors duration-300 hover:bg-[#D4AF37] max-sm:hidden"
-          >
+          <button onClick={onCancelEdit} title="Cancelar edición" className="flex cursor-pointer items-center gap-1 rounded-md px-4 py-2 text-sm ring ring-[#D4AF37] transition-colors duration-300 hover:bg-[#D4AF37] max-sm:hidden">
             <X size={16} /> Cancelar edición
           </button>
         )}
@@ -99,16 +50,9 @@ export const PurchaseForm = () => {
           <TrackingField register={register} errors={errors} />
           <AmountField register={register} errors={errors} />
 
-          <FormButton
-            type="internal"
-            text={`${isEditing ? "Guardar cambios" : "Agregar compra"}`}
-          />
+          <FormButton type="internal" text={`${isEditing ? "Guardar cambios" : "Agregar compra"}`} />
           {isEditing && (
-            <button
-              onClick={onCancelEdit}
-              title="Cancelar edición"
-              className="flex w-full cursor-pointer items-center justify-center gap-1 rounded-md px-4 py-2 text-sm ring ring-[#D4AF37] transition-colors duration-300 hover:bg-[#D4AF37] sm:hidden"
-            >
+            <button onClick={onCancelEdit} title="Cancelar edición" className="flex w-full cursor-pointer items-center justify-center gap-1 rounded-md px-4 py-2 text-sm ring ring-[#D4AF37] transition-colors duration-300 hover:bg-[#D4AF37] sm:hidden">
               <X size={16} /> Cancelar edición
             </button>
           )}

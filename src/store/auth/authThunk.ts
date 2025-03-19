cbeaea83948079c3451b07f02a07933872380036
7R0N1X <techsoftec.tronix@gmail.com>
2025-03-19 11:45:56 -0500

    fix(auth): enhance error handling and improve code formatting in authentication thunks

--- src/store/auth/authThunk.ts
+++ src/store/auth/authThunk.ts
@@ -1,10 +1,10 @@
 import { AppDispatch } from "@store/store";
 import { auth } from "@/firebase/config";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from "firebase/auth";
-import { setAuthenticated, setLogIn, setLogOut } from "./authSlice";
 import { setAnnualQuota, setAvailableBalance, setIsEditing, setPurchaseEdit, setTotalImported } from "@store/user/userSlice";
+import { setAuthenticated, setLogIn, setLogOut } from "./authSlice";
 
-export const createUserWithEmailAndPasswordThunk = (userName: string, userEmail: string, userPassword: string) => {
+export const createUserWithEmailAndPasswordThunk = ( userName: string, userEmail: string, userPassword: string ) => {
   return async (dispatch: AppDispatch) => {
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, userEmail, userPassword);
@@ -12,7 +12,7 @@ export const createUserWithEmailAndPasswordThunk = (userName: string, userEmail:
       const { uid, displayName, email, photoURL } = userCredential.user;
       dispatch(setLogIn({ uid, displayName, email, photoURL }));
     } catch (error) {
-      error instanceof Error && dispatch(setLogIn(error.message));
+      error instanceof Error && dispatch(setLogIn({ errorMessage: error.message }));
     }
   };
 };
@@ -20,12 +20,12 @@ export const createUserWithEmailAndPasswordThunk = (userName: string, userEmail:
 export const signInWithEmailAndPasswordThunk = (userEmail: string, userPassword: string) => {
   return async (dispatch: AppDispatch) => {
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, userEmail, userPassword);
+      const userCredential = await signInWithEmailAndPassword(auth, userEmail, userPassword );
       const { uid, displayName, email, photoURL } = userCredential.user;
       dispatch(setLogIn({ uid, displayName, email, photoURL }));
       dispatch(setAuthenticated(true));
     } catch (error) {
-      error instanceof Error && dispatch(setLogIn(error.message));
+      error instanceof Error && dispatch(setLogIn({ errorMessage: error.message }));
     }
   };
 };
@@ -33,13 +33,13 @@ export const signInWithEmailAndPasswordThunk = (userEmail: string, userPassword:
 export const signInWithGoogleThunk = () => {
   return async (dispatch: AppDispatch) => {
     try {
-      const provider = new GoogleAuthProvider().setCustomParameters({prompt: "select_account"});
+      const provider = new GoogleAuthProvider().setCustomParameters({ prompt: "select_account" });
       const userCredential = await signInWithPopup(auth, provider);
       const { uid, displayName, email, photoURL } = userCredential.user;
       dispatch(setLogIn({ uid, displayName, email, photoURL }));
       dispatch(setAuthenticated(true));
     } catch (error) {
-      error instanceof Error && dispatch(setLogIn(error.message));
+      error instanceof Error && dispatch(setLogIn({ errorMessage: error.message }));
     }
   };
 };
@@ -60,13 +60,13 @@ export const logOut = () => {
       await auth.signOut();
       dispatch(setLogOut());
       dispatch(setAuthenticated(false));
-      dispatch(setPurchaseEdit(""))
-      dispatch(setIsEditing(false))
-      dispatch(setAnnualQuota(0))
-      dispatch(setAvailableBalance())
-      dispatch(setTotalImported([]))
+      dispatch(setPurchaseEdit(""));
+      dispatch(setIsEditing(false));
+      dispatch(setAnnualQuota(0));
+      dispatch(setAvailableBalance());
+      dispatch(setTotalImported([]));
     } catch (error) {
-      error instanceof Error && dispatch(setLogOut(error.message));
+      error instanceof Error && dispatch(setLogOut({ uid: null, displayName: null, email: null, photoURL: null, errorMessage: error.message }));
     }
   };
 };

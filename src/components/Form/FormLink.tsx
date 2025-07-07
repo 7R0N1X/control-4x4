90d390b469c8193228dd2b7517ecd7c748535e2f
7R0N1X <techsoftec.tronix@gmail.com>
2025-07-06 19:36:05 -0500

    feat: add title attribute to improve SEO and accessibility

--- src/components/Form/FormLink.tsx
+++ src/components/Form/FormLink.tsx
@@ -4,14 +4,15 @@ interface FormLinkProps {
   variant: "left" | "center" | "right";
   href: string;
   text: string;
+  title: string;
 }
 
-export const FormLink = ({ variant, href, text }: FormLinkProps) => {
+export const FormLink = ({ variant, href, text, title }: FormLinkProps) => {
   return (
     <div
       className={`flex items-center ${variant === "left" && "justify-start"} ${variant === "center" && "justify-center"} ${variant === "right" && "justify-end"}`}
     >
-      <Link to={href} className="text-right text-sm text-[#9394A5] hover:underline">
+      <Link to={href} title={title} className="text-right text-sm text-[#9394A5] hover:underline">
         {text}
       </Link>
     </div>

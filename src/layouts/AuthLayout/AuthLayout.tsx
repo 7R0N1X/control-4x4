06a441c6f9a53c1cf46599e98abd1bd9ec21c61e
7R0N1X <techsoftec.tronix@gmail.com>
2025-07-06 19:33:36 -0500

    feat: add descriptive alt and title attributes to improve SEO and accessibility

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -11,7 +11,7 @@ export const AuthLayout = ({ title, description, children }: AuthLayout) => {
   return (
     <section className="grid h-[100dvh] grid-cols-1 max-sm:p-4 lg:grid-cols-2">
       <div className="h-full w-full overflow-hidden max-lg:hidden">
-        <img src={img} alt="" className="h-full w-full object-cover" />
+        <img src={img} alt="Mujer sonriente comprando en línea con laptop y tarjeta de crédito." title="Mujer sonriente comprando en línea con laptop y tarjeta de crédito." className="h-full w-full object-cover" />
       </div>
       <div className="flex flex-col items-center justify-center">
         <Logo className="mb-6" />
